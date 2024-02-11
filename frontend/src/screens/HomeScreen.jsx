a1acc3bb554197fa76384325745d663f12e6f60e
kunal-bendale <kunalbendale882000@gmail.com>
2024-02-11 20:51:09 +0530

    Added HomeScreen, ProductScreen, Rating and Product Components

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product.jsx";
+import products from "../products.js";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product key={product._id} product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
