7add8df4017b04ee7d546392251c9969bbdb4d65
kunal-bendale <kunalbendale882000@gmail.com>
2024-03-30 13:44:34 +0530

    Implemented the Redux Toolkit for the State Management and We are making API calls via Redux and Added two new Components Loader and Message for loading and displaying messages on Screens

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
 import Product from "../components/Product.jsx";
 // import products from "../products.js";
+import { useGetProductsQuery } from "../slices/productsApiSlice.js";
+import Loader from "../components/Loader.jsx";
+import Message from "../components/Message.jsx";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
+  /* API Call to getProducts endpoint with axios*/
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await axios.get("http://localhost:8000/api/products");
-      console.log(response);
-      setProducts(response.data);
-    };
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const response = await axios.get("http://localhost:8000/api/products");
+  //     console.log(response);
+  //     setProducts(response.data);
+  //   };
 
-    fetchProducts();
-  }, []);
+  //   fetchProducts();
+  // }, []);
+
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product key={product._id} product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product key={product._id} product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
